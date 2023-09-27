3bb66ae7ea4a0418de6b3a3d3315ed82d3303a27
Marci0107 <marton.acs66@gmail.com>
2023-09-27 19:17:07 +0200

    2. házi

--- src/utils/rand_wrapper.py
+++ src/utils/rand_wrapper.py
@@ -13,12 +13,7 @@ def random_from_list(input_list):
     return input_list[random_no]
 
 def random_sublist_from_list(input_list, number_of_elements):
-    len_input_list = len(input_list)
-    random_numbers_list = []
-    for i in range(number_of_elements):
-        random_no = random.randint(0, len_input_list - 1)
-        random_numbers_list.append(input_list[random_no])
-    return random_numbers_list
+    return random.choices(input_list, k = number_of_elements)
 
 def random_sublist_from_list2(input_list, number_of_elements):
     random_numbers_list = random.sample(input_list, number_of_elements)
@@ -43,6 +38,10 @@ def hundred_large_random():
     return hundred_random_int_numbers
 
 def five_random_number_div_three():
+    list_1 = [value for value in list(range(9, 1001)) if value % 3 == 0]
+    return random.sample(list_1, k = 5)
+
+def five_random_number_div_three2():
     five_random_numbers = []
     while len(five_random_numbers) < 5:
         rand_between_nine_thousand = random.randint(9, 1000)
@@ -51,11 +50,14 @@ def five_random_number_div_three():
     return five_random_numbers
 
 def random_reorder(input_list):
+    return random.sample(input_list, k = len(input_list))
+
+def random_reorder2(input_list):
     shuffled_list = input_list.copy()
     random.shuffle(shuffled_list)
     return shuffled_list
 
 
 def uniform_one_to_five():
-    random_uniform_num = random.uniform(1, 5)
+    random_uniform_num = random.uniform(1, 6)
     return random_uniform_num
